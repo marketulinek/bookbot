5a26c6758996deb74f24befb4fbd5cb3fea94718
MarketaH <habranova.marketa@gmail.com>
2024-03-30 23:06:53 +0100

    My solution for counting letters

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def main():
     num_words = get_num_words(text)
     print(f"{num_words} words found in the book.")
 
+    # Statistics of letter
+    num_letters = get_num_letters(text)
+    print(num_letters)
+
 
 def get_book_text(path_to_book):
     with open(path_to_book) as f:
@@ -20,6 +24,18 @@ def get_book_text(path_to_book):
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+
+def get_num_letters(text):
+    words = text.split()
+    letter_stats = {}
+    for word in words:
+        lowered_word = word.lower()
+        for letter in lowered_word:
+            if letter not in letter_stats:
+                letter_stats[letter] = 0
+            letter_stats[letter] += 1
+    return letter_stats
 
 
 main()
