3f2adcf25d6ca374105b24d59a57e47a9ee7cf4e
MarketaH <habranova.marketa@gmail.com>
2024-03-31 09:33:47 +0200

    My solution of printing the book report

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 def main():
 
     path_to_book = 'books/frankenstein.txt'
-
-    # Print the book
     text = get_book_text(path_to_book)
-    print(text)
-
-    # Count the words
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the book.")
-
-    # Statistics of chars
     num_chars = get_num_chars(text)
-    print(num_chars)
+    char_stats = wrap_dict_by_list(num_chars)
+    char_stats.sort(reverse=True, key=sort_on)
+
+    print(f"--- Begin report of {path_to_book} ---")
+    print(f"{num_words} words found in the document\n")
+
+    for char in char_stats:
+        if char['char'].isalpha():
+            print(f"The '{char['char']}' character was found {char['num']} times")
+
+    print("--- End report ---")
 
 
 def get_book_text(path_to_book):
@@ -35,6 +37,18 @@ def get_num_chars(text):
         else:
             chars[lowered_char] = 1
     return chars
+
+
+def wrap_dict_by_list(dict_chars):
+    new_list = []
+    for char, num in dict_chars.items():
+        new_dict = {'char': char, 'num': num}
+        new_list.append(new_dict)
+    return new_list
+
+
+def sort_on(dict_chars):
+    return dict_chars['num']
 
 
 main()
